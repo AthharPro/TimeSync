ea81adadc3578d81b695b19973a29b929feeabaa
Rashmitha Kaushalya <151978018+rashmithakau@users.noreply.github.com>
2025-11-19 15:03:48 +0530

    fix:type errors

--- libs/shared/src/enums/index.ts
+++ libs/shared/src/enums/index.ts
@@ -1,5 +1,5 @@
 export * from './roles.enum';
-export * from './timesheetStatus.enum';
+export * from './timeSheetStatus.enum';
 export * from './empMenuItem.enum';
 export * from './otherActivity.enum';
 export * from './billableType.enum';
