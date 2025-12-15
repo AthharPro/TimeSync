a546d4a9f0dcb14d9373b10e316b6c052af469b2
Thejana Walisundara <thejanawalisundara@Thejanas-MacBook-Air.local>
2025-12-15 14:30:46 +0530

    fix: resolve merge conflicts and errors

--- apps/ui/src/components/organisms/popup/index.ts
+++ apps/ui/src/components/organisms/popup/index.ts
@@ -1,4 +1,4 @@
-export { default as CreateAccountPopUp } from './CreateAccountPopUp';
+export { default as CreateAccountPopUp } from './CreateAccountPopup';
 export * from './CreateTeamPopUp';
 export * from './ProjectTeamViewPopUp';
 export * from './AddEmployeePopup';
